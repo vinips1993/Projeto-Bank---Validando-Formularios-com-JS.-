df84f49da9ff9a709642f9ba0db08cc97c1834cf
vinips1993 <rm550907@fiap.com.br>
2023-12-09 23:25:42 -0300

    Validando com js

---
+++ js/valida-cpf.js
@@ -0,0 +1,24 @@
+export default function ehUmCPF(campo) {
+    const cpf = campo.value.replace(/\.|-/g, "");
+    validaNumerosReptidos(cpf);
+
+    console.log(validaNumerosReptidos(cpf));
+}
+
+function validaNumerosReptidos(cpf) {
+    const numeroRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    return numeroRepetidos.includes(cpf);
+
+}
